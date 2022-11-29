9bc53e1f3b8935fe745fb6a57e4446de220de37c
Tal Yaron <tal.yaron@gmail.com>
2022-11-29 15:41:23 +0200

    cont

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -36,15 +36,7 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>My amazing app!!!!</h1>
       <div className="card">
         <button onClick={handleAddCounter}>count is {count}</button>
         <button
@@ -59,9 +51,6 @@ function App() {
           }}>
           MULTIPLE COUNTER
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <button onClick={()=>{em.emit('test',count)}}>EMIT</button>
       <p className="read-the-docs">
