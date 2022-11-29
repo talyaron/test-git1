2df999baa181c60da6c7e60b66a0ee0d690ea253
Tal Yaron <tal.yaron@gmail.com>
2022-11-29 12:12:00 +0200

    random 1000

--- example/src/cont/counter.ts
+++ example/src/cont/counter.ts
@@ -1,3 +1,8 @@
 export function addToCounter(count: number, setCount: Function): void {
   setCount(count + 5);
 }
+
+export function getRandomNumber(count: number, setCount: Function):void{
+  setCount(Math.floor( Math.random()*1000))
+}
+
